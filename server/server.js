6cfff93a30f57c0d7c7ee9bb74c1bb00b284871c
Tran Anh Tuan <tuan.trananh@pycogroup.com>
2019-08-20 14:46:30 +0700

    Fix

--- server/server.js
+++ server/server.js
@@ -1,30 +1,26 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
-
-const { mongoURI } = require("./config/key")
-const { port } = require("./config/key")
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { mongoURI } = require('./config/key');
+require('dotenv').config();
 mongoose
   .connect(mongoURI, { useNewUrlParser: true, useCreateIndex: true })
-  .then(console.log("connect to Acexis db"))
-  .catch(console.log())
+  .then(console.log('connect to Acexis db'))
+  .catch(console.log());
 
-const app = express()
+const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  )
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-  next()
-})
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use("/acexis/customer", require("./route/api/customerApi"))
+app.use('/acexis/customer', require('./route/api/customerApi'));
 
-app.listen(port, () => {
-  console.log("Listening on port " + port)
-})
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port ' + process.env.PORT);
+});
