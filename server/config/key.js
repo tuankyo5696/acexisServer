a5e5098ee1118161ec21422eeeabde18fc31cb43
Tran Anh Tuan <tuan.trananh@pycogroup.com>
2019-08-20 13:34:47 +0700

    update

--- server/config/key.js
+++ server/config/key.js
@@ -1,6 +1,7 @@
 const mongoURI =
   "mongodb+srv://hongphuc2197:xiaomi21@cluster0-ydvey.mongodb.net/test?retryWrites=true&w=majority"
-
+const port = 5000
 module.exports = {
-  mongoURI
+  mongoURI,
+  port
 }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 
 const { mongoURI } = require("./config/key")
-
+const { port } = require("./config/key")
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(console.log("connect to Acexis db"))
@@ -20,9 +20,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
   next()
 })
-
-const port = process.env.PORT || 5000
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
